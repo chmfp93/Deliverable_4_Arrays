4abaec620e3495a3f151149023d12e2b612d76e0
cmfig <cmfig@CM-LENOVO-LAPTO>
2023-02-09 03:32:33 -0500

    Add project files.

---
+++ Deliverable(4)/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ Author: Carlos M. Figueroa Palomino
+ Date: 2/9/2023
+ Description: C# console application that computes the first 25 numbers of the Fibonacci sequence.
+*/
+
+using System;
+namespace Deliverable_4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[25];
+
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < 25; i++)
+            {
+                fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+            }
+
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine("Fibonacci number (" + i + ") = " + fibonacci[i]);
+            }
+
+        }
+    }
+}
